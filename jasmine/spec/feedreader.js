b980fddedc6fe71f817d8253121384b5fa3a8cb0
Andre Oliveira <github@aboutandre.com>
2016-09-17 23:58:45 +0200

    Fixes small typos in the Spec file

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -29,7 +29,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* This test loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -40,14 +40,14 @@ $(function() {
                 // Checks it has an "url"
                 expect(allFeeds[i].url).toBeDefined();
                 // Checks that the "url" is not empty
-                expect(allFeeds[i].length).not.toBe(0);
+                expect(allFeeds[i].url.length).not.toBe(0);
                 // Checks that the "url" is a string and not a boolean or number
-                expect(typeof allFeeds[i].name).toBe("string");
-            };
+                expect(typeof allFeeds[i].url).toBe("string");
+            }
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* This test  loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -61,32 +61,25 @@ $(function() {
                 expect(typeof allFeeds[i].name).toBe("string");
                 // Checks that the "name" is not empty
                 expect(allFeeds[i].name.length).not.toBe(0);
-            };
+            }
         });
 
     });
-
-    /* TODO: Write a new test suite named "The menu" */
 
     describe('The menu', function() {
         // Sets two variables to keep the code DRY and short
         var menuToggle = document.querySelector(".menu-icon-link"),
             main = document.body;
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
-
-        it('should be hidden by default', function() {
+        //  This test ensures the menu element is hidden by default.
+                it('should be hidden by default', function() {
             expect(main.className).toContain("menu-hidden");
         });
 
-        /* TODO: Write a test that ensures the menu changes
+        /* This test ensures the menu changes
          * visibility when the menu icon is clicked. This test
-         * should have two expectations: does the menu display when
-         * clicked and does it hide when clicked again.
+         * has two expectations: does the menu display when
+         * clicked and does it hide when clicked again?
          */
 
         it('should toggle the visibility of the menu', function() {
@@ -100,18 +93,15 @@ $(function() {
             menuToggle.click();
             expect(main.className).toContain("menu-hidden");
 
-        })
+        });
 
     });
-    /* TODO: Write a new test suite named "Initial Entries" */
 
     describe('Initial Entries', function() {
 
-        /* TODO: Write a test that ensures when the loadFeed
+        /* This test ensures that when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
         // Keeping the code DRY
@@ -128,13 +118,10 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
-
     describe('New Feed Selection', function() {
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* This test ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
          */
 
         //Sets the current feed to be compared to the new feed later
